fa17c33e02a47ce8baca897e742c6bb92adb3b5d
xingliu5210 <169063561+xingliu5210@users.noreply.github.com>
2025-11-15 10:11:39 -0500

    Initial Commit

---
+++ Final_RecipeLab_Team16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final_RecipeLab_Team16
+//
+//  Created by Erikson on 11/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Final_RecipeLab_Team16Tests/Final_RecipeLab_Team16Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Final_RecipeLab_Team16Tests.swift
+//  Final_RecipeLab_Team16Tests
+//
+//  Created by Erikson on 11/15/25.
+//
+
+import Testing
+@testable import Final_RecipeLab_Team16
+
+struct Final_RecipeLab_Team16Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

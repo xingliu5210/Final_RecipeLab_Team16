709c96402e8ec0f841b1862bccede8d4c6877bd4
Ching-Chieh-Wang <immactavish@gmail.com>
2025-11-16 19:05:57 -0500

    feat: skeleton

---
+++ Final_RecipeLab_Team16/Auth/Login/LoginController.swift
@@ -0,0 +1,7 @@
+//
+//  LoginController.swift
+//  Final_RecipeLab_Team16
+//
+//  Created by 王敬捷 on 11/16/25.
+//
+
---
+++ Final_RecipeLab_Team16/Auth/Login/LoginView.swift
@@ -0,0 +1,7 @@
+//
+//  LoginView.swift
+//  Final_RecipeLab_Team16
+//
+//  Created by 王敬捷 on 11/16/25.
+//
+

522bcf5580d51f81d5a25b96b35cfb6a3c34c846
Ching-Chieh-Wang <immactavish@gmail.com>
2025-11-22 16:30:08 -0500

    feat: addReipie page and my recipie page (file only)

--- Final_RecipeLab_Team16/AddRecipiePage/AddRecipiePageController.swift
+++ Final_RecipeLab_Team16/AddRecipiePage/AddRecipiePageController.swift
@@ -5,3 +5,12 @@
 //  Created by 王敬捷 on 11/16/25.
 //
 
+import UIKit
+
+class AddRecipiePageController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Create Recipie"
+    }
+}
--- Final_RecipeLab_Team16/MyRecipiePage/MyRecipiePageController.swift
+++ Final_RecipeLab_Team16/MyRecipiePage/MyRecipiePageController.swift
@@ -4,17 +4,13 @@
 //
 //  Created by 王敬捷 on 11/16/25.
 //
-<<<<<<< Updated upstream
 
-class AddRecipiePageController: UIViewController {
-=======
 import UIKit
 
 class MyRecipiePageController: UIViewController {
->>>>>>> Stashed changes
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        title = "Home"
+        title = "My Recipie"
     }
 }

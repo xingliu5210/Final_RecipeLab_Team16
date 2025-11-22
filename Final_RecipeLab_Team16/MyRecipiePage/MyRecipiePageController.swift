aea73e2ee467c75cae663b6c8617d4523901dade
Ching-Chieh-Wang <immactavish@gmail.com>
2025-11-22 15:53:17 -0500

    feat: buttonNav

---
+++ Final_RecipeLab_Team16/MyRecipiePage/MyRecipiePageController.swift
@@ -0,0 +1,14 @@
+//
+//  AddRecipiePageController.swift
+//  Final_RecipeLab_Team16
+//
+//  Created by 王敬捷 on 11/16/25.
+//
+
+class AddRecipiePageController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        title = "Home"
+    }
+}

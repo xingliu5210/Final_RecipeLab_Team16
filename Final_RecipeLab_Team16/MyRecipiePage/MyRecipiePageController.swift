3b1341090abc1d3bab3c464e307780debd978dae
Ching-Chieh-Wang <immactavish@gmail.com>
2025-11-22 16:27:33 -0500

    feat: tab controller

--- Final_RecipeLab_Team16/MyRecipiePage/MyRecipiePageController.swift
+++ Final_RecipeLab_Team16/MyRecipiePage/MyRecipiePageController.swift
@@ -4,8 +4,14 @@
 //
 //  Created by 王敬捷 on 11/16/25.
 //
+<<<<<<< Updated upstream
 
 class AddRecipiePageController: UIViewController {
+=======
+import UIKit
+
+class MyRecipiePageController: UIViewController {
+>>>>>>> Stashed changes
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white

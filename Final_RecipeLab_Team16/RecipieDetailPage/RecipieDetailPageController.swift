5751cc4ff72618b276c86642bf5143f2b7fb0616
Ching-Chieh-Wang <immactavish@gmail.com>
2025-12-05 16:08:03 +0800

    fix : recipeDetailPage not scrollable issue

--- Final_RecipeLab_Team16/RecipieDetailPage/RecipieDetailPageController.swift
+++ Final_RecipeLab_Team16/RecipieDetailPage/RecipieDetailPageController.swift
@@ -21,14 +21,8 @@ class RecipieDetailPageController: BaseViewController {
 
     private let rootView = RecipieDetailPageView()
 
-    override func loadView() {
-        // Use our detail view directly as the controller's view
-        view = rootView
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do NOT set title here (to avoid the black "Recipe" text)
-        // rootView already configures itself with fake data & scrolling
+        setContent(rootView)
     }
 }

6d94eb8c0736ac13a04a707433e2751e6ea7c4b0
Ching-Chieh-Wang <immactavish@gmail.com>
2025-12-05 16:22:37 +0800

    fix: back button white

--- Final_RecipeLab_Team16/RecipieDetailPage/RecipieDetailPageController.swift
+++ Final_RecipeLab_Team16/RecipieDetailPage/RecipieDetailPageController.swift
@@ -4,16 +4,6 @@
 //
 //  Created by 王敬捷 on 11/16/25.
 //
-/*
-import UIKit
-
-class RecipieDetailPageController: BaseViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        title = "My Recipie"
-    }
-}*/
 
 import UIKit
 
